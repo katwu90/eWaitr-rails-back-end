a62332289079191900c9d28f96067152b5c25359
katwu90 <katwu90@gmail.com>
2017-11-01 15:07:09 -0400

    Migrates to schema
    Completes migration for menu_item resource with the
    `bin/rails db:migrate` command.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101150119) do
+ActiveRecord::Schema.define(version: 20171101160148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20171101150119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "price", null: false
+    t.string "food_category"
+    t.bigint "menu_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
   end
 
   create_table "menus", force: :cascade do |t|
@@ -43,5 +54,6 @@ ActiveRecord::Schema.define(version: 20171101150119) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "menu_items", "menus"
   add_foreign_key "menus", "users"
 end
