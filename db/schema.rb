c683544151c3a2fe898958fb3de87b69396b7c3d
katwu90 <katwu90@gmail.com>
2017-11-01 21:56:32 -0400

    Migrate files to schema
    - `bin/rails db:migrate` migrates the migration file generated from
      orders resource to schema of the data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101201233) do
+ActiveRecord::Schema.define(version: 20171102013400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(version: 20171101201233) do
     t.index ["user_id"], name: "index_menus_on_user_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "menu_item_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+    t.index ["menu_item_id"], name: "index_orders_on_menu_item_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "token", null: false
@@ -68,4 +78,6 @@ ActiveRecord::Schema.define(version: 20171101201233) do
   add_foreign_key "examples", "users"
   add_foreign_key "menu_items", "menus"
   add_foreign_key "menus", "users"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "orders", "menu_items"
 end
