0ff5bdee88fdd64bd17d3d28a7cd69b4eb856fac
katwu90 <katwu90@gmail.com>
2017-11-02 09:45:26 -0400

    Modified controller to be OpenRead
    Modified orders controller to be OpenRead and require a current user for
    create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
-class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :update, :destroy]
+class OrdersController < OpenReadController
+  before_action :set_order, only: %i[update destroy]
 
   # GET /orders
   def index
@@ -10,15 +10,15 @@ class OrdersController < ApplicationController
 
   # GET /orders/1
   def show
-    render json: @order
+    render json: Order.find(params[:id])
   end
 
   # POST /orders
   def create
-    @order = Order.new(order_params)
+    @order = current_user.orders.build(order_params)
 
     if @order.save
-      render json: @order, status: :created, location: @order
+      render json: @order, status: :created
     else
       render json: @order.errors, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   def update
     if @order.update(order_params)
-      render json: @order
+      head :no_content
     else
       render json: @order.errors, status: :unprocessable_entity
     end
@@ -36,12 +36,14 @@ class OrdersController < ApplicationController
   # DELETE /orders/1
   def destroy
     @order.destroy
+
+    head :no_content
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = current_user.orders.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
